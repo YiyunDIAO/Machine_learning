9b7d55bfb1a7c183990cf6465596772e467e83d6
YiyunDIAO <60147216+YiyunDIAO@users.noreply.github.com>
2024-12-11 21:01:48 -0600

    Update utils.py
    Added data prep and train-test split function

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
 from IPython.display import display 
+import pandas as pd 
+import numpy as np 
+import os 
+pd.set_option('display.max_columns', None) 
+pd.set_option('display.max_rows', None)
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
 class ml_dataset: 
     ## this is used to get a dataset as a class in order to achieve basic manipulation 
     def __init__(self, dataframe, target_variable):
@@ -45,3 +54,52 @@ class ml_dataset:
         self.correlation_matrix() 
         print('----------------- Summary of Categorical Variable ---------------------')
         self.categorical_summary() 
+
+    def fill_null(self, config = {'loan_int_rate': 'mean', 'person_emp_length': 'indicator'}): 
+        """
+        This is the function to fill na based on certain config 
+        config: an input dictionary where we specify different ways to fill na 
+        there can be a few ways: 
+            - mean: simply fill na with mean, apply with continuous variable 
+            - zero: fill na with 0 
+            - indicator: fill na with mean, but create another indicator saying this is null 
+        """
+        for var in config.keys(): 
+            method = config[var] 
+            if method == 'mean': 
+                self.dataset[var] = self.dataset[var].fillna(self.dataset[var].mean())
+                                         
+            elif method == 'zero': 
+                self.dataset[var] = self.dataset[var].fillna(0)
+
+            elif method == 'indicator': 
+                self.dataset[var+'_missing'] = self.dataset[var].isna()
+                self.dataset[var] = self.dataset[var].fillna(self.dataset[var].mean())
+                
+        display(self.df_summary) 
+        
+    def training_prep(self, split = {'train': 0.6, 'test': 0.2, 'validation': 0.2}, target = 'loan_status'): 
+        """
+        This function will automatically do: 1) Onehot encoding; 2) Train-Validation-Test Split 
+        it will return train, test, validation split 
+        
+        """
+        oneHot = pd.get_dummies(self.dataset[self.categorical_variables], drop_first = True)
+
+        df_prepped = pd.concat([self.dataset[self.continuous_variables], oneHot], axis=1)
+
+        y = df_prepped[target] 
+        X = df_prepped.drop(target, axis=1) 
+
+        train_size = split['train'] 
+        test_size = split['test'] 
+        validation_size = split['validation'] 
+
+        # split out validation first 
+        X_train_test, X_validate, y_train_test, y_validate = train_test_split(X, y, test_size=validation_size, random_state=42)
+
+        # then split out train/test 
+        X_train, X_test, y_train, y_test = train_test_split(X_train_test, y_train_test, test_size=test_size/(train_size + test_size), random_state=42)
+        
+        
+        return X_train, X_test, X_validate, y_train, y_test, y_validate
